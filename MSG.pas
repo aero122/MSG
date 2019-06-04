8fce94cc62cef2e705d776ccc46fafd7dc5ce6c9
aero122 <35301089+aero122@users.noreply.github.com>
2019-06-04 11:37:10 +0500

    Progress bar added

--- MSG.pas
+++ MSG.pas
@@ -4,7 +4,7 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, ShellAPI;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, ShellAPI, Vcl.ComCtrls;
 
 type
   TForm1 = class(TForm)
@@ -16,6 +16,7 @@ type
     Edit1: TEdit;
     Button3: TButton;
     Button4: TButton;
+    ProgressBar1: TProgressBar;
     procedure Button1Click(Sender: TObject);
     procedure ScanNet();
     procedure Button2Click(Sender: TObject);
